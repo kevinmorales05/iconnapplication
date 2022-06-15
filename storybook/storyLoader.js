2ba4aa2c626b5c0b06eca3684136d50bebf5502a
mariano.martinez <mariano.martinez>
2022-06-15 02:19:05 -0500

    develop: Container component + stories.

--- storybook/storyLoader.js
+++ storybook/storyLoader.js
@@ -4,10 +4,14 @@
 // https://github.com/elderfo/react-native-storybook-loader.git
 
 function loadStories() {
+  require('../src/components/atoms/Container/Container.stories');
   require('../src/components/molecules/Button/Button.stories');
 }
 
-const stories = ['../src/components/molecules/Button/Button.stories'];
+const stories = [
+  '../src/components/atoms/Container/Container.stories',
+  '../src/components/molecules/Button/Button.stories'
+];
 
 module.exports = {
   loadStories,
