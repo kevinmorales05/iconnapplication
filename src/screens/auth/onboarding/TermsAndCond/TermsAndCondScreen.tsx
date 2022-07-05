1a5ef011f9374a82ec2518a821669fd72dab58a2
mariano.martinez <mariano.martinez>
2022-07-05 15:40:22 -0500

    develop: TermsAndCond improvements on layout.

--- src/screens/auth/onboarding/TermsAndCond/TermsAndCondScreen.tsx
+++ src/screens/auth/onboarding/TermsAndCond/TermsAndCondScreen.tsx
@@ -1,12 +1,13 @@
-import { Image, ScrollView, StyleSheet } from 'react-native';
+import { Image, ScrollView } from 'react-native';
 import React, { useEffect, useState } from 'react';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import theme from 'components/theme/theme';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-import { Checkbox, Container } from 'components/atoms';
+import { ActionButton, Checkbox, Container } from 'components/atoms';
 import { TextContainer, Button } from 'components/molecules';
 import { ICONN_DOCUMENT_EDIT } from 'assets/images';
-import { RootState, setTermsAndCond, useAppDispatch, useAppSelector } from 'rtk';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import { setTermsAndCond, useAppDispatch } from 'rtk';
 
 interface Props {
   onSubmit: () => void;
@@ -22,109 +23,98 @@ const TermsAndCondScreen: React.FC<Props> = ({ onSubmit, goBack }) => {
     dispatch(setTermsAndCond({termsAndConditions: value}));
     return;
   }, [value])
-  
+
   return (
     <ScrollView
       bounces={false}
-      style={{ flex: 1, flexDirection: 'column' }}
+      style={{ flex: 1 }}
       contentContainerStyle={{
-        justifyContent: 'space-between',
         flexGrow: 1,
-        paddingBottom: insets.bottom,
-        paddingTop: insets.top,
-        width: '100%'
+        paddingBottom: insets.bottom + 16,
+        paddingTop: insets.top
       }}
     >
       <Container>
         <Container flex style={{ marginTop: 32, marginLeft: 16 }}>
-        <Image source={ICONN_DOCUMENT_EDIT}  style={{height: 48, width:48}}/>
+          <Image source={ICONN_DOCUMENT_EDIT} style={{ height: 48, width: 48 }} />
         </Container>
         <TextContainer
           text="Términos y Condiciones"
           typography="h2"
-          fontBold={true}
-          marginLeft={16}
+          fontBold
+          marginLeft={6}
           marginTop={20}
         />
         <TextContainer
           text="Confirmo que:"
           typography="placeholder"
           textColor={theme.fontColor.paragraph}
-          marginLeft={16}
+          marginLeft={6}
           marginTop={20}
         />
-        <Container
-          flex
-          style={{ flexDirection: 'row', marginLeft: 16, marginTop: 14 }}
-        >
+        <Container flex style={{ flexDirection: 'row', marginLeft: 6, marginTop: 14 }}>
           <Container style={{ marginTop: 6 }}>
-            <Icon name="circle" size={6} color="#dadadb" />
+            <Icon name="circle" size={7} color="#dadadb" />
           </Container>
           <TextContainer text="Revisé los " marginLeft={16} />
           <TextContainer
             text="Términos de uso"
             typography="link"
             textColor={theme.fontColor.link}
-            underline={true}
-            fontBold={true}
+            underline
+            fontBold
           />
         </Container>
-        <Container
-          flex
-          style={{ flexDirection: 'row', marginLeft: 16, marginTop: 14 }}
-        >
+        <Container flex style={{ flexDirection: 'row', marginLeft: 6, marginTop: 14 }}>
           <Container style={{ marginTop: 6 }}>
-            <Icon name="circle" size={6} color="#dadadb" />
+            <Icon name="circle" size={7} color="#dadadb" />
           </Container>
           <TextContainer text="Leí el " marginLeft={16} />
           <TextContainer
             text="Aviso de privacidad"
             typography="link"
             textColor={theme.fontColor.link}
-            underline={true}
-            fontBold={true}
+            underline
+            fontBold
           />
         </Container>
-        <Container
-          flex
-          style={{ flexDirection: 'row', marginLeft: 16, marginTop: 14 }}
-        >
+
+        <Container flex style={{ flexDirection: 'row', marginLeft: 6, marginTop: 14 }}>
           <Container style={{ marginTop: 6 }}>
-            <Icon name="circle" size={6} color="#dadadb" />
+            <Icon name="circle" size={7} color="#dadadb" />
           </Container>
           <TextContainer text="Soy mayor de 18 años" marginLeft={16} />
         </Container>
       </Container>
-      <Container crossCenter center row style={{ backgroundColor: theme.brandColor.iconn_warm_grey }} height={71}>
-        <Checkbox checked={value} onPress={() => setCheckBoxValue(!value)}>Acepto todo lo anterior</Checkbox>
+      
+      <Container crossCenter center row style={{ backgroundColor: theme.brandColor.iconn_warm_grey, marginTop: 320 }} height={71}>
+        <Checkbox size='xxlarge' checked={value} onPress={() => setCheckBoxValue(!value)}>Acepto todo lo anterior</Checkbox>
       </Container>
-      <Container
-        row
-        style={{ marginBottom: 24, justifyContent: 'space-around' }}
-      >
-        <Button
-          icon={<Icon name="arrow-left" size={24} />}
-          leftIconStyle={styles.iconIconButton}
+      
+      <Container flex row crossAlignment="end" space="between">
+        <ActionButton
+          size="large"
           onPress={goBack}
-          color={'iconn_light_grey'}
-          round={true}
-          textStyle={styles.iconButton}
-        >
-          {' '}
-          {}
-        </Button>
-        <Button
-          disabled={!value}
-          round
-          onPress={onSubmit}
-          rightIcon={
-            <Icon
-              name="arrow-right"
+          color="iconn_med_grey"
+          icon={
+            <AntDesign
+              name="arrowleft"
               size={24}
-              color={theme.brandColor.iconn_white}
+              color={theme.fontColor.dark}
             />
           }
-          textStyle={styles.textIconButton}
+        />
+
+        <Button
+          length="short"
+          round
+          disabled={!value}
+          onPress={onSubmit}
+          fontSize="h4"
+          fontBold
+          size='large'
+          style={{ marginTop: 8 }}
+          rightIcon={<AntDesign name="arrowright" size={24} color="white" />}
         >
           Siguiente
         </Button>
@@ -132,18 +122,5 @@ const TermsAndCondScreen: React.FC<Props> = ({ onSubmit, goBack }) => {
     </ScrollView>
   );
 };
-
-const styles = StyleSheet.create({
-  textIconButton: {
-    marginRight: 8,
-    fontSize: theme.fontSize.h4
-  },
-  iconButton: {
-    marginLeft: 0
-  },
-  iconIconButton: {
-    marginLeft: 20
-  }
-});
 
 export default TermsAndCondScreen;
