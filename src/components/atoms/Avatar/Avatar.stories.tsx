8af002dffc90a3859eb4e0abaf182ff951219036
mariano.martinez <mariano.martinez>
2022-07-11 09:07:41 -0500

    develop: atoms/Avatar component added.

---
+++ src/components/atoms/Avatar/Avatar.stories.tsx
@@ -0,0 +1,85 @@
+import BufferView from 'utils/BufferView';
+import { storiesOf } from '@storybook/react-native';
+import React from 'react';
+import Avatar from './Avatar';
+import { action } from '@storybook/addon-actions';
+
+storiesOf('Atoms/Avatar', module)
+  .addDecorator(BufferView)
+  .add('Size', () => {
+    return (
+      <>
+        <Avatar size="xxsmall" onPress={() => {}}>
+          Hello Avatar
+        </Avatar>
+        <Avatar size="xsmall" onPress={() => {}}>
+          Hello Avatar
+        </Avatar>
+        <Avatar size="small" onPress={() => {}}>
+          Hello Avatar
+        </Avatar>
+        <Avatar size="medium" onPress={() => {}}>
+          Hello Avatar
+        </Avatar>
+        <Avatar size="large" onPress={() => {}}>
+          Hello Avatar
+        </Avatar>
+        <Avatar size="xlarge" onPress={() => {}}>
+          Hello Avatar
+        </Avatar>
+        <Avatar size="xxlarge" onPress={() => {}}>
+          Hello Avatar
+        </Avatar>
+      </>
+    );
+  })
+  .add('Type', () => {
+    return (
+      <>
+        <Avatar
+        title="MM"
+        onPress={action('pressed')}
+      />
+      <Avatar
+        square
+        title="MM"
+        onPress={action('pressed')}
+      />
+      <Avatar
+        rounded
+        title="MM"
+        onPress={action('pressed')}
+      />
+      </>
+    );
+  })
+  .add('Title', () => {
+    return (
+      <>
+        <Avatar title="MM" />
+      </>
+    );
+  })
+  .add('Source', () => {
+    return (
+      <>
+        <Avatar source={{ uri: 'https://www.audi.com.mx/content/dam/nemo/models/a3/rs-3-limousine/my-2022/NeMo-Derivate-Startpage/stage/1080x1920-audi-rs-3-sedan-stage-mobile-RS3_2021_3182.jpg?imwidth=768'}}/>
+      </>
+    );
+  })
+  .add('Editable', () => {
+    return (
+      <>
+        <Avatar
+        title="MM"
+        editable={true}
+        onPress={action("pressed")}
+        />
+        <Avatar
+          source={{ uri: 'https://www.audi.com.mx/content/dam/nemo/models/a3/rs-3-limousine/my-2022/NeMo-Derivate-Startpage/stage/1080x1920-audi-rs-3-sedan-stage-mobile-RS3_2021_3182.jpg?imwidth=768'}}
+          editable={true}
+          onPress={action("pressed")}
+        />
+      </>
+    );
+  });
--- storybook/storyLoader.js
+++ storybook/storyLoader.js
@@ -4,6 +4,7 @@
 // https://github.com/elderfo/react-native-storybook-loader.git
 
 function loadStories() {
+  require('../src/components/atoms/Avatar/Avatar.stories');
   require('../src/components/atoms/Checkbox/Checkbox.stories');
   require('../src/components/atoms/Container/Container.stories');
   require('../src/components/molecules/Button/Button.stories');
@@ -11,6 +12,7 @@ function loadStories() {
 }
 
 const stories = [
+  '../src/components/atoms/Avatar/Avatar.stories',
   '../src/components/atoms/Checkbox/Checkbox.stories',
   '../src/components/atoms/Container/Container.stories',
   '../src/components/molecules/Button/Button.stories',
