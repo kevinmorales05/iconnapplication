29599de1cc42926a5f2913d613f768a32a8694a7
mariano.martinez <mariano.martinez>
2022-07-15 15:22:23 -0500

    develop: icorrect password & blocked account cases managed.

--- src/rtk/thunks/auth.thunks.ts
+++ src/rtk/thunks/auth.thunks.ts
@@ -52,28 +52,12 @@ createAsyncThunk('auth/signUpUserWithEmailAndPasswordThunk', async (payload: {em
 
 /**
  * Firebase (email + password) signin method
+ * It's better return full promise without resolving it.
+ * TODO: You should change the other firebase methods as this:
  */
 export const signInWithEmailAndPasswordThunk =
 createAsyncThunk('auth/signInWithEmailAndPasswordThunk', async (payload: {email: string, pass: string}) => {
-  if (payload.email === ""){
-    console.log("Petición incompleta, el campo de correo electrónico es obligatorio.")
-  }
-  try {
-    const userInfo = await auth().signInWithEmailAndPassword(payload.email, payload.pass);
-    return userInfo;
-  } catch (error) {
-    if (error.code === 'auth/wrong-password') {
-      console.log('Contraseña incorrecta.');
-    }
-    if (error.code === 'auth/too-many-requests'){
-      console.log("Hemos bloqueado su cuenta de manera temporal debido a actividad inusual, intente más tarde. Puede activar inmediatamente reseteando su contraseña.")
-      //We have blocked all requests from this device due to unusual activity. Try again later. [ Access to this account has been temporarily disabled due to many failed login attempts. You can immediately restore it by resetting your password or you can try again later.
-    }
-    console.log("error integro ", error)
-    return error;
-  }
-  
-  
+  return auth().signInWithEmailAndPassword(payload.email, payload.pass);
 });
 
 /**
