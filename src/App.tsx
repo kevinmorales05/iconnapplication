81112ede606141a0cd3ec69a22c77859ce0d82b4
mariano.martinez <mariano.martinez>
2022-07-17 18:31:16 -0500

    develop: own toast component with vertical transformation, with context integration, nice! + old toast library removed.

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,19 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import NavContainer from 'navigation/NavContainer';
 import { store } from 'rtk';
-import { AlertContextProvider, LoadingContextProvider } from 'context';
+import { AlertContextProvider, LoadingContextProvider, ToastContextProvider } from 'context';
+import { Toast } from 'context';
 
 const App: React.FC = () => (
   <Provider store={store}>
-    <LoadingContextProvider>
-      <AlertContextProvider>
-        <NavContainer />
-      </AlertContextProvider>
-    </LoadingContextProvider>
+    <ToastContextProvider>
+    <Toast />
+      <LoadingContextProvider>
+        <AlertContextProvider>
+          <NavContainer />
+        </AlertContextProvider>
+      </LoadingContextProvider>
+    </ToastContextProvider>
   </Provider>
 );
 
--- src/context/index.ts
+++ src/context/index.ts
@@ -1,2 +1,4 @@
 export * from './alert.context';
 export * from './loading.context';
+export * from './toast.context';
+export * from './Toast'
