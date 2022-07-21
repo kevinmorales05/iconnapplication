e6b3e8400c1dde198a3fc86720cc9211501a28ed
mariano.martinez <mariano.martinez>
2022-07-21 10:17:24 -0500

    develop: 79285, 78565 fixed issues + input component restore.

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -2,26 +2,25 @@ import ICONN_BINOMIO_LOGOS from './iconn-binomio.webp';
 import ICONN_BINOMIO_LOGOS_LIGHT from './iconn-binomio-logos-light.webp';
 import ICONN_BACKGROUND_IMAGE from './iconn-background-image.webp';
 import ICONN_EMAIL from './iconn-email.webp';
-import ICONN_EYE from './iconn-eye.webp';
+import ICONN_EYE_ON from './iconn-eye.webp';
 import ICONN_COFFEE from './iconn-coffee.webp';
 import ICONN_DOCUMENT_EDIT from './iconn-document-edit.webp';
 import ICONN_WARNING from './iconn-warning.webp';
 import ICONN_SUCCESS from './iconn-success.webp';
 import ICONN_ERROR from './iconn-error.webp';
-import ICONN_EYE_VISIBLE from './iconn-eye-visible.webp';
-import ICONN_INVITE from './iconn-signin.webp'
-
+import ICONN_INVITE from './iconn-signin.webp';
+import ICONN_EYE_OFF from './iconn-eye-visible.webp';
 export {
   ICONN_BINOMIO_LOGOS,
   ICONN_BINOMIO_LOGOS_LIGHT,
   ICONN_BACKGROUND_IMAGE,
   ICONN_EMAIL,
-  ICONN_EYE,
+  ICONN_EYE_ON,
   ICONN_COFFEE,
   ICONN_DOCUMENT_EDIT,
   ICONN_WARNING,
   ICONN_SUCCESS,
   ICONN_ERROR,
-  ICONN_EYE_VISIBLE,
-  ICONN_INVITE
+  ICONN_INVITE,
+  ICONN_EYE_OFF
 };
--- src/components/atoms/Input/Input.tsx
+++ src/components/atoms/Input/Input.tsx
@@ -17,7 +17,7 @@ import { Container } from '../Container';
 import { TextContainer } from '../../molecules/TextContainer';
 import { ActionButton } from '../ActionButton';
 import Ionicons from 'react-native-vector-icons/Ionicons';
-import { ICONN_BACKGROUND_IMAGE, ICONN_EYE } from 'assets/images';
+import { ICONN_EYE_ON, ICONN_EYE_OFF } from 'assets/images';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import { CustomText } from '../CustomText';
 
@@ -46,7 +46,6 @@ export interface Props {
   name: string;
   control: Control<FieldValues, object>;
   rules?: RegisterOptions;
-  renderErrorIcon?: boolean;
   testID?: string;
   sufixOutIcon?: boolean;
   onPressInfo?: any;
@@ -81,7 +80,6 @@ const Input = forwardRef(({
   name,
   control,
   rules,
-  renderErrorIcon = true,
   testID,
   sufixOutIcon = false,
   onPressInfo,
@@ -91,7 +89,7 @@ const Input = forwardRef(({
   phone = false
 }: Props, ref: ForwardedRef<any>) => {
   const {
-    inputStyle, inputContainerStyle, passwordImageStyle, errorImageStyle, prefixImageStyle
+    inputStyle, inputContainerStyle, passwordImageStyle, prefixImageStyle
   } = styles;
   const [secureTextEntry, setSecureTextEntry] = useState<boolean>(passwordField || false);
   const [focused, setFocused] = useState<boolean>(false);
@@ -196,11 +194,8 @@ const Input = forwardRef(({
           </Container>
           {passwordField && showPasswordEnable && (
             <Touchable testID={`${testID}-hide-password`} onPress={() => setSecureTextEntry(!secureTextEntry)} opacityEffect>
-              <Image source={ICONN_EYE} style={passwordImageStyle} />
+              <Image source={secureTextEntry ? ICONN_EYE_ON : ICONN_EYE_OFF} style={passwordImageStyle} />
             </Touchable>
-          )}
-          {!(passwordField && showPasswordEnable) && !!error && renderErrorIcon && (
-            <Image testID={`${testID}-error-image`} source={ICONN_BACKGROUND_IMAGE} style={errorImageStyle} />
           )}
           {datePicker && (
             <ActionButton size='xsmall' color='' onPress={onPressDatePickerIcon!}
@@ -221,7 +216,7 @@ const Input = forwardRef(({
         )}
       </Container>
       
-      {!!error && (
+      {(!!error && error.length > 1) && (
       <TextContainer
         testID={`${testID}-error-label`}
         text={error}
@@ -239,6 +234,7 @@ const styles = StyleSheet.create({
   inputStyle: {
     fontSize: theme.fontSize.paragraph,
     paddingHorizontal: 12,
+    width: '100%',
     color: theme.fontColor.dark
   },
   inputContainerStyle: {
@@ -247,14 +243,8 @@ const styles = StyleSheet.create({
     borderColor: theme.fontColor.medgrey
   },
   passwordImageStyle: {
-    width: 20,
-    height: 16,
-    marginRight: 15,
-    marginVertical: 15
-  },
-  errorImageStyle: {
-    width: 10,
-    height: 14,
+    width: 24,
+    height: 24,
     marginRight: 15,
     marginVertical: 15
   },
