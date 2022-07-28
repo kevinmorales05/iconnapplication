0733883bfc57ba724ed537d94976e85bdbb30332
mariano.martinez <mariano.martinez>
2022-07-28 10:52:36 -0500

    develop: 79625 InvoiceScreen layout completed.

--- src/screens/home/billing/Invoice/InvoiceScreen.tsx
+++ src/screens/home/billing/Invoice/InvoiceScreen.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useRef } from 'react';
 import { ScrollView, TextInput } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import AntDesign from 'react-native-vector-icons/AntDesign';
-import { ActionButton, Input, TextContainer, Button, Container } from 'components';
+import { ActionButton, Input, TextContainer, Button, Container, CardBilling } from 'components';
 import theme from 'components/theme/theme';
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
 import { emailRules } from 'utils/rules';
@@ -47,9 +47,8 @@ const InvoiceScreen: React.FC<Props> = ({ title, onSubmit, goBack, onPressInfo }
         paddingBottom: insets.bottom + 16
       }}
     >
-      <TextContainer typography="h2" fontBold text="Datos fiscales" marginTop={34} marginBottom={8} />
-
-      <Container backgroundColor={theme.brandColor.iconn_white} height={80} style={{paddingHorizontal:16}}>
+      <TextContainer typography="h2" fontBold text="Datos fiscales" marginTop={20} marginBottom={8} />
+      <Container backgroundColor={theme.brandColor.iconn_white} height={90} style={{ paddingHorizontal: 16, borderTopStartRadius: 8, borderTopEndRadius: 8, marginTop: 4 }}>
         <Button
           length="long"
           round
@@ -57,34 +56,22 @@ const InvoiceScreen: React.FC<Props> = ({ title, onSubmit, goBack, onPressInfo }
           onPress={handleSubmit(submit)}
           fontSize="h4"
           fontBold
-          style={{ marginTop: 8 }}
-          rightIcon={<AntDesign name="arrowright" size={24} color="white" />}
+          marginTop={16}
         >
-          Siguiente
+          Crear perfil fiscal
         </Button>
       </Container>
-      <Container backgroundColor={theme.brandColor.iconn_info} center crossCenter>
-        <TextContainer  marginVertical={27} text='Deberás verificar tu correo para facturar.' />
+      <Container backgroundColor={'#DDE8F3'} center crossCenter height={58} style={{ borderBottomEndRadius: 8, borderBottomStartRadius: 8}}>
+        <TextContainer text='Deberás verificar tu correo para facturar.' />
       </Container>
-
-      
-      
-
-      <Container flex row crossAlignment="end" space="between">
-        <ActionButton size="large" onPress={goBack} color="iconn_med_grey" icon={<AntDesign name="arrowleft" size={24} color={theme.fontColor.dark} />} />
-
-        <Button
-          length="short"
-          round
-          disabled={!isValid}
-          onPress={handleSubmit(submit)}
-          fontSize="h4"
-          fontBold
-          style={{ marginTop: 8 }}
-          rightIcon={<AntDesign name="arrowright" size={24} color="white" />}
-        >
-          Siguiente
-        </Button>
+      <Container style={{ marginTop: 34 }} >
+        <TextContainer text='Nueva Factura' typography='h3' fontBold />
+      </Container>
+      <Container style={{ marginTop: 18 }}>
+        <CardBilling text='Facturar ticket' type='seven' disable={false} onPress={() => { }} />
+      </Container>
+      <Container style={{ marginTop: 8 }}>
+        <CardBilling text='Facturar ticket' type='petro' disable={false} onPress={() => { }} />
       </Container>
     </ScrollView>
   );
