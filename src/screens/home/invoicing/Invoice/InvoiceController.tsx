2f2e621c4c39166a9bc491570ef3e98af6050301
mariano.martinez <mariano.martinez>
2022-08-05 16:30:40 -0500

    develop: resendEmailVerification endpoint added + Modal fixes.

--- src/screens/home/invoicing/Invoice/InvoiceController.tsx
+++ src/screens/home/invoicing/Invoice/InvoiceController.tsx
@@ -1,28 +1,66 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { HomeStackParams } from 'navigation/types';
 import InvoiceScreen from './InvoiceScreen';
 import { SafeArea } from 'components/atoms/SafeArea';
-import { InvoicingProfileInterface, RootState, useAppSelector } from 'rtk';
+import { InvoicingProfileInterface, RootState, useAppDispatch, useAppSelector } from 'rtk';
 import theme from 'components/theme/theme';
+import { resendVerificationEmailThunk } from 'rtk/thunks/invoicing.thunks';
+import { useAlert, useLoading } from 'context';
 
 const InvoiceController: React.FC = () => {
+  const dispatch = useAppDispatch();
   const { navigate } = useNavigation<NativeStackNavigationProp<HomeStackParams>>();
   const { invoicingProfileList } = useAppSelector((state: RootState) => state.invoicing);
+  const { loading } = useAppSelector((state: RootState) => state.invoicing);
   const onSubmit = async () => navigate('CreateTaxProfile');
-
+  const alert = useAlert();
+  const loader = useLoading();
   let defaultProfile: InvoicingProfileInterface;
 
   if (invoicingProfileList.length === 1) {
     defaultProfile = invoicingProfileList[0];
   } else {
-    defaultProfile = invoicingProfileList.find((obj) => obj.default === true)!;
-  }  
+    defaultProfile = invoicingProfileList.find(obj => obj.default === true)!;
+  }
+
+  useEffect(() => {
+    if (loading === false) {
+      loader.hide();
+    }
+  }, [loading]);
+
+  const showAlert = () => {
+    alert.show(
+      {
+        title: 'Verifica tu correo electronico',
+        message: `Para facturar sólo falta verificar tu correo. Revisa tu bandeja de entrada:`,
+        acceptTitle: 'Aceptar',
+        secondMessage: defaultProfile.email,
+        onAccept() {
+          alert.hide();
+        }
+      },
+      'success'
+    );
+  };
+
+  const resendEmail = async () => {
+    loader.show();
+    try {
+      const response = await dispatch(resendVerificationEmailThunk(defaultProfile.email)).unwrap();
+      if (response.responseCode === 200) {
+        showAlert();
+      }
+    } catch (error) {
+      console.warn(error);
+    }
+  };
 
   return (
     <SafeArea topSafeArea={false} bottomSafeArea={false} barStyle="dark" backgroundColor={theme.brandColor.iconn_background}>
-      <InvoiceScreen invoicingProfileList={invoicingProfileList} defaultProfile={defaultProfile!} onSubmit={onSubmit} />
+      <InvoiceScreen invoicingProfileList={invoicingProfileList} defaultProfile={defaultProfile!} onSubmit={onSubmit} resendEmail={resendEmail} />
     </SafeArea>
   );
 };
