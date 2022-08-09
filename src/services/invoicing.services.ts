29af89d714c621b0299395c62f391b3ebc891861
mariano.martinez <mariano.martinez>
2022-08-09 15:35:46 -0500

    develop: BASE_URL changed for Invoicing services.

--- src/services/invoicing.services.ts
+++ src/services/invoicing.services.ts
@@ -5,8 +5,8 @@ import { InvoicingProfileInterface } from 'rtk/types/invoicing.types';
  * Function to get regimens list
  */
 async function getTaxRegimeList(): Promise<any> {
-  const response = await InvoicingApi.getInstance().getRequest('/taxRegime/list');
-  if (response === undefined) return Promise.reject(new Error('getTaxRegimeList:/taxRegime/list'));
+  const response = await InvoicingApi.getInstance().getRequest('/invoicing/taxRegime/list');
+  if (response === undefined) return Promise.reject(new Error('getTaxRegimeList:/invoicing/taxRegime/list'));
   const { data } = response;
   return data;
 }
@@ -15,8 +15,8 @@ async function getTaxRegimeList(): Promise<any> {
  * Function to get CFDI list
  */
 async function getCFDIList(): Promise<any> {
-  const response = await InvoicingApi.getInstance().getRequest('/cfdi/list');
-  if (response === undefined) return Promise.reject(new Error('getCFDIList:/cfdi/list'));
+  const response = await InvoicingApi.getInstance().getRequest('/invoicing/cfdi/list');
+  if (response === undefined) return Promise.reject(new Error('getCFDIList:/invoicing/cfdi/list'));
   const { data } = response;
   return data;
 }
@@ -26,8 +26,8 @@ async function getCFDIList(): Promise<any> {
  * @param postalCode
  */
 async function getColonies(postalCode: string): Promise<any> {
-  const response = await InvoicingApi.getInstance().getRequest(`/colony/list/${postalCode}`);
-  if (response === undefined) return Promise.reject(new Error(`getColonies:/colony/list/${postalCode}`));
+  const response = await InvoicingApi.getInstance().getRequest(`/invoicing/colony/list/${postalCode}`);
+  if (response === undefined) return Promise.reject(new Error(`getColonies:/invoicing/colony/list/${postalCode}`));
   const { data } = response;
   return data;
 }
@@ -37,8 +37,8 @@ async function getColonies(postalCode: string): Promise<any> {
  * @param invoicingProfile
  */
 async function registerInvoicingProfile(invoicingProfile: any): Promise<any> {
-  const response = await InvoicingApi.getInstance().postRequest('/invoicingProfile/create', invoicingProfile);
-  if (response === undefined) return Promise.reject(new Error('registerInvoicingProfile:/invoicingProfile/create'));
+  const response = await InvoicingApi.getInstance().postRequest('/invoicing/invoicingProfile/create', invoicingProfile);
+  if (response === undefined) return Promise.reject(new Error('registerInvoicingProfile:/invoicing/invoicingProfile/create'));
   const { data } = response;
   return data;
 }
@@ -47,8 +47,8 @@ async function registerInvoicingProfile(invoicingProfile: any): Promise<any> {
  * Function to get invoicing profile list
  */
 async function getInvoicingProfileList(uid: string): Promise<any> {
-  const response = await InvoicingApi.getInstance().getRequest(`/invoicingProfile/list/${uid}`);
-  if (response === undefined) return Promise.reject(new Error('getInvoicingProfileList:/invoicingProfile/list'));
+  const response = await InvoicingApi.getInstance().getRequest(`/invoicing/invoicingProfile/list/${uid}`);
+  if (response === undefined) return Promise.reject(new Error('getInvoicingProfileList:/invoicing/invoicingProfile/list'));
   const { data } = response;
   return data;
 }
@@ -61,8 +61,8 @@ async function deleteInvoicingProfile(invoicingProfile: InvoicingProfileInterfac
   const { invoicing_profile_id } = invoicingProfile;
   invoicingProfile.status = false;
   /* const emptInvoiceP = {} as InvoicingProfileInterface; */
-  const response = await InvoicingApi.getInstance().putRequest(`/invoicingProfile/delete/${invoicing_profile_id}`, invoicingProfile);
-  if (response === undefined) return Promise.reject(new Error(`deleteInvoicingProfile:invoicingProfile/delete/${invoicing_profile_id}`));
+  const response = await InvoicingApi.getInstance().putRequest(`/invoicing/invoicingProfile/delete/${invoicing_profile_id}`, invoicingProfile);
+  if (response === undefined) return Promise.reject(new Error(`deleteInvoicingProfile:/invoicing/invoicingProfile/delete/${invoicing_profile_id}`));
   const { data } = response;
   return data;
 }
@@ -73,8 +73,8 @@ async function deleteInvoicingProfile(invoicingProfile: InvoicingProfileInterfac
 
 async function updateInvoicingProfile(invoicingProfile: InvoicingProfileInterface): Promise<any> {
   const { invoicing_profile_id } = invoicingProfile;
-  const response = await InvoicingApi.getInstance().putRequest(`/invoicingProfile/update/${invoicing_profile_id}`, invoicingProfile);
-  if (response === undefined) return Promise.reject(new Error(`updateInvoicingProfile:invoicingProfile/update/${invoicing_profile_id}`));
+  const response = await InvoicingApi.getInstance().putRequest(`/invoicing/invoicingProfile/update/${invoicing_profile_id}`, invoicingProfile);
+  if (response === undefined) return Promise.reject(new Error(`updateInvoicingProfile:/invoicing/invoicingProfile/update/${invoicing_profile_id}`));
   const { data } = response;
   return data;
 }
@@ -83,8 +83,8 @@ async function updateInvoicingProfile(invoicingProfile: InvoicingProfileInterfac
  * Function to resend verification email
  */
  async function resendVerificationEmail(email: string): Promise<any> {
-  const response = await InvoicingApi.getInstance().getRequest(`/mail/sendEmailForVerification/${email}`);
-  if (response === undefined) return Promise.reject(new Error(`resendVerificationEmail:mail/sendEmailForVerification/${email}`));
+  const response = await InvoicingApi.getInstance().getRequest(`/invoicing/mail/sendEmailForVerification/${email}`);
+  if (response === undefined) return Promise.reject(new Error(`resendVerificationEmail:/invoicing/mail/sendEmailForVerification/${email}`));
   const { data } = response;
   return data;
 }
