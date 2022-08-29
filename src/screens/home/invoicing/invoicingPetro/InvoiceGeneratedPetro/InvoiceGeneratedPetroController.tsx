28ab3aef2d6ab2d5f9df10ed1ae5d2f59999ef1d
mariano.martinez <mariano.martinez>
2022-08-29 11:36:28 -0500

    feature71656: #80706 ResendInvoice organism component added and implemented.

--- src/screens/home/invoicing/invoicingPetro/InvoiceGeneratedPetro/InvoiceGeneratedPetroController.tsx
+++ src/screens/home/invoicing/invoicingPetro/InvoiceGeneratedPetro/InvoiceGeneratedPetroController.tsx
@@ -1,16 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import { SafeArea } from 'components/atoms/SafeArea';
-import { InvoiceGeneratedScreen } from 'components';
+import { InvoiceGeneratedScreen, ResendInvoice } from 'components';
 import theme from 'components/theme/theme';
 import { useNavigation } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { HomeStackParams } from 'navigation/types';
 import { InvoicingProfileInterface, RootState, useAppSelector } from 'rtk';
+import { FieldValues } from 'react-hook-form';
 
 const InvoiceGeneratedPetroController: React.FC<any> = ({ route }) => {
   const { navigate } = useNavigation<NativeStackNavigationProp<HomeStackParams>>();
   const { invoicingProfileList } = useAppSelector((state: RootState) => state.invoicing);
   const [defaultProfile, setDefaultProfile] = useState<InvoicingProfileInterface | null>(null);
+  const [resendInvoiceVisible, setResendInvoiceVisible] = useState<boolean>(false);
 
   useEffect(() => {
     setDefaultProfile(
@@ -23,9 +25,11 @@ const InvoiceGeneratedPetroController: React.FC<any> = ({ route }) => {
   const goToHome = () => navigate('Home');
   const goToNewInvoice = () => navigate('AddTicketPetro', { ticket: undefined, position: undefined });
   const goToViewerPDF = () => navigate('ViewInvoiceGeneratedPetro', { invoiceGenerated: route.params ? route.params.invoiceGenerated : undefined });
+  const onPressOut = () => setResendInvoiceVisible(false);
+  const onPressResend = () => setResendInvoiceVisible(true);
 
-  const resendGeneratedInvoice = () => {
-    console.log('Aqui se va a reenviar la factura Petro....');
+  const resendGeneratedInvoice = (fields: FieldValues) => {
+    console.log('Aqui se va a reenviar la factura Petro....', fields);
     // TODO integrate endpoint to resend generated invoice.
   };
 
@@ -33,11 +37,18 @@ const InvoiceGeneratedPetroController: React.FC<any> = ({ route }) => {
     <SafeArea barStyle="dark" topSafeArea={false} bottomSafeArea={false} backgroundColor={theme.brandColor.iconn_background}>
       <InvoiceGeneratedScreen
         viewGeneratedInvoice={goToViewerPDF}
-        resendGeneratedInvoice={resendGeneratedInvoice}
+        resendGeneratedInvoice={onPressResend}
         finalize={goToHome}
         newInvoice={goToNewInvoice}
         defaultProfile={defaultProfile!}
         invoiceGenerated={route.params ? route.params.invoiceGenerated : undefined}
+      />
+      <ResendInvoice
+        onPressOut={onPressOut}
+        visible={resendInvoiceVisible}
+        message={`Reenviaremos esta factura a tu email registrado.`}
+        secondMessage={`Puedes agregar otros correos adicionales separándolos con un espacio.`}
+        resend={resendGeneratedInvoice}
       />
     </SafeArea>
   );
--- src/screens/home/invoicing/invoicingSeven/InvoiceGeneratedSeven/InvoiceGeneratedSevenController.tsx
+++ src/screens/home/invoicing/invoicingSeven/InvoiceGeneratedSeven/InvoiceGeneratedSevenController.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { SafeArea } from 'components/atoms/SafeArea';
-import { InvoiceGeneratedScreen } from 'components';
+import { InvoiceGeneratedScreen, ResendInvoice } from 'components';
 import theme from 'components/theme/theme';
 import { useNavigation } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
@@ -11,6 +11,7 @@ const InvoiceGeneratedSevenController: React.FC<any> = ({ route }) => {
   const { navigate } = useNavigation<NativeStackNavigationProp<HomeStackParams>>();
   const { invoicingProfileList } = useAppSelector((state: RootState) => state.invoicing);
   const [defaultProfile, setDefaultProfile] = useState<InvoicingProfileInterface | null>(null);
+  const [resendInvoiceVisible, setResendInvoiceVisible] = useState<boolean>(false);
 
   useEffect(() => {
     setDefaultProfile(
@@ -23,6 +24,8 @@ const InvoiceGeneratedSevenController: React.FC<any> = ({ route }) => {
   const goToHome = () => navigate('Home');
   const goToNewInvoice = () => navigate('AddTicketSeven', { ticket: undefined, position: undefined });
   const goToViewerPDF = () => navigate('ViewInvoiceGeneratedSeven', { invoiceGenerated: route.params ? route.params.invoiceGenerated : undefined });
+  const onPressOut = () => setResendInvoiceVisible(false);
+  const onPressResend = () => setResendInvoiceVisible(true);
 
   const resendGeneratedInvoice = () => {
     console.log('Aqui se va a reenviar la factura Seven....');
@@ -33,11 +36,18 @@ const InvoiceGeneratedSevenController: React.FC<any> = ({ route }) => {
     <SafeArea barStyle="dark" topSafeArea={false} bottomSafeArea={false} backgroundColor={theme.brandColor.iconn_background}>
       <InvoiceGeneratedScreen
         viewGeneratedInvoice={goToViewerPDF}
-        resendGeneratedInvoice={resendGeneratedInvoice}
+        resendGeneratedInvoice={onPressResend}
         finalize={goToHome}
         newInvoice={goToNewInvoice}
         defaultProfile={defaultProfile!}
         invoiceGenerated={route.params ? route.params.invoiceGenerated : undefined}
+      />
+      <ResendInvoice
+        onPressOut={onPressOut}
+        visible={resendInvoiceVisible}
+        message={`Reenviaremos esta factura a tu email registrado.`}
+        secondMessage={`Puedes agregar otros correos adicionales separándolos con un espacio.`}
+        resend={resendGeneratedInvoice}
       />
     </SafeArea>
   );
