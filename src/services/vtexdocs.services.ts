3727881e8b1d43b99161e16f1c6f6121853fbabb
Kevin Morales <kevin.morales05@epn.edu.ec>
2022-09-02 10:06:36 -0500

    #82372 Add endpoint to get one doc

--- src/services/vtexdocs.services.ts
+++ src/services/vtexdocs.services.ts
@@ -1,7 +1,5 @@
 import { InvoicingApi } from '../http/api-invoicing';
-import { DocsApi } from '../http/api-docs'
-
-
+import { DocsApi } from '../http/api-docs';
 
 /**
  * Function to create doc
@@ -9,12 +7,24 @@ import { DocsApi } from '../http/api-docs'
  * TODO interface for doc
  */
 
-async function createDoc(dataentity:string, doc:any) : Promise<any> {
-    const response = await DocsApi.getInstance().postRequest(`/${dataentity}/documents?_schema=schema`, doc);
-    const { data } = response;
-    return data;
+async function createDoc(dataentity: string, doc: any): Promise<any> {
+  const response = await DocsApi.getInstance().postRequest(`/${dataentity}/documents?_schema=schema`, doc);
+  const { data } = response;
+  return data;
 }
 
+/**
+ * Function to get one doc of the docs
+ * TODO error handling
+ * TODO interface for doc
+ */
+
+async function getDocByDocID(dataentity: string, docID: string): Promise<any> {
+  const response = await DocsApi.getInstance().getRequest(`/dataentities/${dataentity}/search?_fields=_all&_where=id=${docID}`);
+  //error handling
+  const { data } = response;
+  return data;
+}
 
 /**
  * Function to get all of the docs
@@ -22,24 +32,11 @@ async function createDoc(dataentity:string, doc:any) : Promise<any> {
  * TODO interface for doc
  */
 
- async function getDocByUserID(dataentity:string, userId:string): Promise<any> {
-    const response = await DocsApi.getInstance().getRequest(`/dataentities/${dataentity}/search?_fields=_all&_where=userId=${userId}`);
-    //error handling
-    const { data } = response;
-    return data;
-}
-
-/**
- * Function to get all of the docs
- * TODO error handling
- * TODO interface for doc
- */
-
- async function getAllDocByUserID(dataentity:string, userId:string): Promise<any> {
-    const response = await DocsApi.getInstance().getRequest(`/dataentities/${dataentity}/search?_fields=_all&_where=id=${userId}`);
-    //error handling
-    const { data } = response;
-    return data;
+async function getAllDocByUserID(dataentity: string, userId: string): Promise<any> {
+  const response = await DocsApi.getInstance().getRequest(`/dataentities/${dataentity}/search?_fields=_all&_where=id=${userId}`);
+  //error handling
+  const { data } = response;
+  return data;
 }
 
 /**
@@ -47,11 +44,11 @@ async function createDoc(dataentity:string, doc:any) : Promise<any> {
  * TODO error handling
  * TODO interface for doc
  */
- async function deleteDocByUserID(dataentity:string, docId:string): Promise<any> {
-    const response = await DocsApi.getInstance().getRequest(`dataentities/${dataentity}/documents/${docId}`);
-    //error handling}
-    const { data } = response;
-    return data;
+async function deleteDocByDocID(dataentity: string, docId: string): Promise<any> {
+  const response = await DocsApi.getInstance().getRequest(`dataentities/${dataentity}/documents/${docId}`);
+  //error handling}
+  const { data } = response;
+  return data;
 }
 /**
  * Function to  update one doc
@@ -61,16 +58,17 @@ async function createDoc(dataentity:string, doc:any) : Promise<any> {
 
 //dataentities/AD/documents/:id
 
-async function updateDocByUserID(dataentity:string, docId:string, doc:any): Promise<any> {
-    const response = await DocsApi.getInstance().patchRequest(`dataentities/${dataentity}/documents/${docId}`, doc);
-    //error handling}
-    const { data } = response;
-    return data;
+async function updateDocByDocID(dataentity: string, docId: string, doc: any): Promise<any> {
+  const response = await DocsApi.getInstance().patchRequest(`dataentities/${dataentity}/documents/${docId}`, doc);
+  //error handling}
+  const { data } = response;
+  return data;
 }
 
-
-
-
 export const invoicingServices = {
-  createDoc, getAllDocByUserID, deleteDocByUserID, updateDocByUserID, getDocByUserID
+  createDoc,
+  getAllDocByUserID,
+  deleteDocByDocID,
+  updateDocByDocID,
+  getDocByUserID
 };
