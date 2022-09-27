75a93ce713b33b1df21e0666945559b53081c8e5
mariano.martinez <mariano.martinez>
2022-09-27 14:14:46 -0500

    feature71656: AboutUsScreen + social networks section.

--- src/screens/home/myAccount/aboutUs/AboutUsController.tsx
+++ src/screens/home/myAccount/aboutUs/AboutUsController.tsx
@@ -4,6 +4,30 @@ import React from 'react';
 import AboutUsScreen from './AboutUsScreen';
 
 const AboutUsController: React.FC = () => {
+  const onPressSevenFacebook = () => {
+    console.log('onPressSevenFacebook...');
+  };
+  const onPressSevenTwitter = () => {
+    console.log('onPressSevenTwitter...');
+  };
+  const onPressSevenPlay = () => {
+    console.log('onPressSevenPlay...');
+  };
+  const onPressSevenInstagram = () => {
+    console.log('onPressSevenInstagram...');
+  };
+  const onPressPetroFacebook = () => {
+    console.log('onPressPetroFacebook...');
+  };
+  const onPressPetroTwitter = () => {
+    console.log('onPressPetroTwitter...');
+  };
+  const onPressPetroPlay = () => {
+    console.log('onPressPetroPlay...');
+  };
+  const onPressPetroInstagram = () => {
+    console.log('onPressPetroInstagram...');
+  };
   return (
     <SafeArea
       childrenContainerStyle={{ paddingHorizontal: 0 }}
@@ -12,7 +36,16 @@ const AboutUsController: React.FC = () => {
       backgroundColor={theme.brandColor.iconn_white}
       barStyle="dark"
     >
-      <AboutUsScreen />
+      <AboutUsScreen
+        onPressSevenFacebook={onPressSevenFacebook}
+        onPressSevenTwitter={onPressSevenTwitter}
+        onPressSevenPlay={onPressSevenPlay}
+        onPressSevenInstagram={onPressSevenInstagram}
+        onPressPetroFacebook={onPressPetroFacebook}
+        onPressPetroTwitter={onPressPetroTwitter}
+        onPressPetroPlay={onPressPetroPlay}
+        onPressPetroInstagram={onPressPetroInstagram}
+      />
     </SafeArea>
   );
 };
--- src/screens/home/myAccount/aboutUs/AboutUsScreen.tsx
+++ src/screens/home/myAccount/aboutUs/AboutUsScreen.tsx
@@ -1,13 +1,34 @@
 import React from 'react';
 import { ScrollView } from 'react-native';
-import { NavigationMenuItem, Container } from 'components';
+import { NavigationMenuItem, Container, TextContainer, Touchable } from 'components';
 import { useNavigation } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { HomeStackParams } from 'navigation/types';
+import EvilIcons from 'react-native-vector-icons/EvilIcons';
+import theme from 'components/theme/theme';
+import Feather from 'react-native-vector-icons/Feather';
 
-interface AboutUsScreenProps {}
+interface AboutUsScreenProps {
+  onPressSevenFacebook: () => void;
+  onPressSevenTwitter: () => void;
+  onPressSevenPlay: () => void;
+  onPressSevenInstagram: () => void;
+  onPressPetroFacebook: () => void;
+  onPressPetroTwitter: () => void;
+  onPressPetroPlay: () => void;
+  onPressPetroInstagram: () => void;
+}
 
-const AboutUsScreen: React.FC<AboutUsScreenProps> = () => {
+const AboutUsScreen: React.FC<AboutUsScreenProps> = ({
+  onPressSevenFacebook,
+  onPressSevenTwitter,
+  onPressSevenPlay,
+  onPressSevenInstagram,
+  onPressPetroFacebook,
+  onPressPetroTwitter,
+  onPressPetroPlay,
+  onPressPetroInstagram
+}) => {
   const { navigate } = useNavigation<NativeStackNavigationProp<HomeStackParams>>();
 
   return (
@@ -24,7 +45,7 @@ const AboutUsScreen: React.FC<AboutUsScreenProps> = () => {
           text="Legal"
           disable={false}
           onPressNavigateTo={() => {
-            navigate('Legal')
+            navigate('Legal');
           }}
         />
         <NavigationMenuItem
@@ -49,6 +70,44 @@ const AboutUsScreen: React.FC<AboutUsScreenProps> = () => {
             console.log('Petro-7.com.mx...');
           }}
         />
+        <Container row crossCenter style={{ marginTop: 38 }}>
+          <TextContainer text="Síguenos como" typography="h6" textAlign="center" />
+          <TextContainer text="7-Eleven" typography="h6" fontBold marginLeft={2} textAlign="center" />
+          <TextContainer text="en:" typography="h6" marginLeft={3} textAlign="center" />
+        </Container>
+        <Container row crossCenter style={{ marginTop: 12 }}>
+          <Touchable onPress={onPressSevenFacebook}>
+            <EvilIcons name="sc-facebook" size={28} color={theme.brandColor.iconn_dark_grey} />
+          </Touchable>
+          <Touchable onPress={onPressSevenTwitter}>
+            <Feather name="twitter" size={24} color={theme.brandColor.iconn_dark_grey} style={{ marginLeft: 20 }} />
+          </Touchable>
+          <Touchable onPress={onPressSevenPlay}>
+            <Feather name="play" size={24} color={theme.brandColor.iconn_dark_grey} style={{ marginLeft: 20 }} />
+          </Touchable>
+          <Touchable onPress={onPressSevenInstagram}>
+            <Feather name="instagram" size={24} color={theme.brandColor.iconn_dark_grey} style={{ marginLeft: 20 }} />
+          </Touchable>
+        </Container>
+        <Container row crossCenter style={{ marginTop: 32 }}>
+          <TextContainer text="Síguenos como" typography="h6" textAlign="center" />
+          <TextContainer text="Petro Seven" typography="h6" fontBold marginLeft={2} textAlign="center" />
+          <TextContainer text="en:" typography="h6" marginLeft={3} textAlign="center" />
+        </Container>
+        <Container row crossCenter style={{ marginTop: 12 }}>
+          <Touchable onPress={onPressPetroFacebook}>
+            <EvilIcons name="sc-facebook" size={28} color={theme.brandColor.iconn_dark_grey} />
+          </Touchable>
+          <Touchable onPress={onPressPetroTwitter}>
+            <Feather name="twitter" size={24} color={theme.brandColor.iconn_dark_grey} style={{ marginLeft: 20 }} />
+          </Touchable>
+          <Touchable onPress={onPressPetroPlay}>
+            <Feather name="play" size={24} color={theme.brandColor.iconn_dark_grey} style={{ marginLeft: 20 }} />
+          </Touchable>
+          <Touchable onPress={onPressPetroInstagram}>
+            <Feather name="instagram" size={24} color={theme.brandColor.iconn_dark_grey} style={{ marginLeft: 20 }} />
+          </Touchable>
+        </Container>
       </Container>
     </ScrollView>
   );
