b94a7bdb72e26f3f93ecbbc1de403125e35e4461
Kevin Morales <kevin.morales05@epn.edu.ec>
2022-10-22 04:35:54 +0900

    Correct issue with the performance of the products rendering

--- src/services/vtex-home.services.ts
+++ src/services/vtex-home.services.ts
@@ -5,7 +5,7 @@ import { DocsApi } from '../http/api-docs';
  */
 async function getHomeItems(dataentity: string): Promise<any> {
   const response = await DocsApi.getInstance().getRequest(
-    `/dataentities/${dataentity}/scroll?_fields=id,description,image,link,navigationType,promotion_name,promotion_type,screeen,status`
+    `/dataentities/${dataentity}/search?_fields=id,description,image,link,navigationType,promotion_name,promotion_type,screeen,status`
   );
   const { data } = response;
   return data;
