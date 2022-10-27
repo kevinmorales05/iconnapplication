e1d3b4f2a2aa94d3898393db775d4fac33d82bb2
Alberto Pena <alberto.pena@citi.com.mx>
2022-10-27 16:20:07 -0500

    feature71656: promotions fix

--- src/screens/home/promotions/PromotionsScreen.tsx
+++ src/screens/home/promotions/PromotionsScreen.tsx
@@ -63,10 +63,8 @@ const PromotionsScreen: React.FC<Props> = ({ onPressClose }) => {
   };
 
   const onPressTab = (cateogry: TabItem) => {
-    /*    if (cateogry.id) {
-          console.log('[onPressTab]', cateogry);
-          setIdCategorySelected(cateogry.id);
-        }*/
+    console.log('seleccionada');
+    console.log(cateogry);
   };
 
   const loadMoreItem = async () => {
@@ -165,7 +163,6 @@ const PromotionsScreen: React.FC<Props> = ({ onPressClose }) => {
       <Container style={styles.containerHeader}>
         <SearchBar isButton onPressSearch={onPressBack} onChangeTextSearch={() => { }} />
         <Container style={{ marginTop: moderateScale(10) }}>
-          <TabAnimatable items={promotionsCategory} onPressItem={onPressTab} />
         </Container>
       </Container>
       <Container width={'100%'} style={{ paddingHorizontal: moderateScale(15) }}>
