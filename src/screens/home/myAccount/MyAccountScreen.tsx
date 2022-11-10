398beb94083830e5c5936474aa612856e644daaa
Kevin Morales <kevin.morales05@epn.edu.ec>
2022-11-11 00:37:47 +0900

    New version generated, the wallet feature was hidden

--- src/screens/home/myAccount/MyAccountScreen.tsx
+++ src/screens/home/myAccount/MyAccountScreen.tsx
@@ -120,7 +120,7 @@ const MyAccountScreen: React.FC<HomeScreenProps> = ({ logOut, onPressVersion, ap
           disable={false}
           icon={<WalletSvg size={moderateScale(24)} />}
           onPressNavigateTo={() => {
-            navigate('WalletStack')
+            inConstruction.show(true);
           }}
         />
         <NavigationMenuItem
