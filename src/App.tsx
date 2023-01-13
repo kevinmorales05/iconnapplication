1347e2b650cbb67b512fd4b3754375c9b8a4feb5
ValerieNamuche <55290243+ValerieNamuche@users.noreply.github.com>
2023-01-13 17:44:49 -0500

    TA #90253: Firebase Analytics and Notif implementations

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Provider } from 'react-redux';
 import NavContainer from 'navigation/NavContainer';
 import { store } from 'rtk';
@@ -16,6 +16,7 @@ import { PersistGate } from 'redux-persist/integration/react';
 import { PermissionsProvider } from 'context/permissions.context';
 import { enableLatestRenderer } from 'react-native-maps';
 import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
+import { NotificationListener, requestUserPermission } from 'utils/notification-helper';
 
 enableLatestRenderer();
 // import Config from 'react-native-config';
@@ -25,29 +26,36 @@ enableLatestRenderer();
 
 let persistor = persistStore(store);
 
-const App: React.FC = () => (
-  <Provider store={store}>
-    <PersistGate persistor={persistor}>
-      <PermissionsProvider>
-        <ToastContextProvider>
-          <Toast />
-          <LoadingContextProvider>
-            <EnterModalContextProvider>
-              <WelcomeModalContextProvider>
-                <InConstructionContextProvider>
-                  <AlertContextProvider>
-                    <BottomSheetModalProvider>
-                      <NavContainer />
-                    </BottomSheetModalProvider>
-                  </AlertContextProvider>
-                </InConstructionContextProvider>
-              </WelcomeModalContextProvider>
-            </EnterModalContextProvider>
-          </LoadingContextProvider>
-        </ToastContextProvider>
-      </PermissionsProvider>
-    </PersistGate>
-  </Provider>
-);
+const App: React.FC = () => {
+  useEffect(() => {
+    requestUserPermission();
+    NotificationListener();
+  }, []);
+
+  return (
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <PermissionsProvider>
+          <ToastContextProvider>
+            <Toast />
+            <LoadingContextProvider>
+              <EnterModalContextProvider>
+                <WelcomeModalContextProvider>
+                  <InConstructionContextProvider>
+                    <AlertContextProvider>
+                      <BottomSheetModalProvider>
+                        <NavContainer />
+                      </BottomSheetModalProvider>
+                    </AlertContextProvider>
+                  </InConstructionContextProvider>
+                </WelcomeModalContextProvider>
+              </EnterModalContextProvider>
+            </LoadingContextProvider>
+          </ToastContextProvider>
+        </PermissionsProvider>
+      </PersistGate>
+    </Provider>
+  );
+};
 
 export default App;
