c6f04850b0d6fa86344075a65bb1035acce4c0c8
Pablo Larios <juan.larios@mediamonks.com>
2023-02-10 10:59:36 -0600

    TA #92429: fix codesmells

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
  */
 
 import { AppRegistry, Text } from 'react-native';
-// import { LogBox } from 'react-native';
 import App from './src/App';
 import { name as appName } from './app.json';
 import './global.js';
@@ -13,12 +12,8 @@ import messaging from '@react-native-firebase/messaging';
 Text.defaultProps = Text.defaultProps || {};
 Text.defaultProps.allowFontScaling = false;
 
-// LogBox.ignoreAllLogs();
-
 // Important setup. If you dont add this gestureHandlerRootHOC wrap to App, then gesture doesnt work for android.
 messaging().setBackgroundMessageHandler(async remoteMessage => {
   //console.log('Message handled in the background!', remoteMessage);
 });
 AppRegistry.registerComponent(appName, () => gestureHandlerRootHOC(App));
-
-//export { default } from './storybook';
